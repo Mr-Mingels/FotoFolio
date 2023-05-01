013efa0767affd4e64e51794aa746cc1923ac772
Pierre <Pierre.Mingels@protonmail.com>
2023-05-02 01:07:24 +0200

    updated both server side code and vercel.json file

--- src/server/server.js
+++ src/server/server.js
@@ -95,6 +95,14 @@ app.get('/api/image', async (req, res) => {
 
 
 
+app.use(express.static(path.join(__dirname, '../../build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../build', 'index.html'));
+});
+
+
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
