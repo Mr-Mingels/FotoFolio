a398b3a1e512cfd97f2a3a5ea046a59a0e246642
Pierre <Pierre.Mingels@protonmail.com>
2023-05-01 23:22:22 +0200

    added a build folder

--- src/server/server.js
+++ src/server/server.js
@@ -9,8 +9,14 @@ const path = require('path');
 dotenv.config();
 
 const app = express();
-app.use(cors({ origin: 'https://foto-folio.vercel.app/' }));
+app.use(cors({ origin: 'https://foto-folio.vercel.app' }));
 app.use(bodyParser.json())
+
+app.use(express.static(path.join(__dirname, '../../build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../build', 'index.html'));
+});
 
 
 
