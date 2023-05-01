31ca6f77ee8855f509445538930858db7df5e200
Pierre <Pierre.Mingels@protonmail.com>
2023-05-01 16:06:26 +0200

    updated my server to fix where the build folder was being pointed to

--- src/server/server.js
+++ src/server/server.js
@@ -12,13 +12,12 @@ const app = express();
 app.use(cors({ origin: 'http://localhost:3000' }));
 app.use(bodyParser.json())
 
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, '../build')));
+app.use(express.static(path.join(__dirname, '../', 'build')));
 
-// Handle all other routes to serve the index.html file
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../', 'build', 'index.html'));
 });
+
 
 
 const PORT = process.env.PORT || 3001;
