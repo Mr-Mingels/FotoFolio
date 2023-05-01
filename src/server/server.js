3b51def6dd79201afc377fcb66a5c0b2c1888f32
Pierre <Pierre.Mingels@protonmail.com>
2023-05-01 23:02:51 +0200

    added a vercel.json file so vercel can communicate with my server file

--- src/server/server.js
+++ src/server/server.js
@@ -12,12 +12,6 @@ const app = express();
 app.use(cors({ origin: 'https://foto-folio.vercel.app/' }));
 app.use(bodyParser.json())
 
-app.use(express.static(path.join(__dirname, '../../build')));
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../build', 'index.html'));
-});
-
 
 
 
