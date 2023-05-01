5a0254c8c213c5d776be3f460e73be770e2168e6
Pierre <Pierre.Mingels@protonmail.com>
2023-05-02 01:13:21 +0200

    updated server side code

--- src/server/server.js
+++ src/server/server.js
@@ -95,10 +95,10 @@ app.get('/api/image', async (req, res) => {
 
 
 
-app.use(express.static(path.join(__dirname, '../../build')));
+app.use(express.static(path.join(__dirname, '../../../build')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../../../build', 'index.html'));
 });
 
 
