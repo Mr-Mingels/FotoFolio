b84409bfae473eb2383b4d4a8c4509d768348d2c
Pierre <Pierre.Mingels@protonmail.com>
2023-05-02 01:27:15 +0200

    trying to fix error

--- src/server/server.js
+++ src/server/server.js
@@ -93,7 +93,11 @@ app.get('/api/image', async (req, res) => {
 });
 
 
+app.use(express.static(path.join(__dirname, '../../build')));
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../build', 'index.html'));
+});
 
 
 app.listen(PORT, () => {
