e6798a519eab5a6fec96982b1b98f8678dd1f0cb
Pierre <Pierre.Mingels@protonmail.com>
2023-05-02 01:19:32 +0200

    error fixing

--- src/server/server.js
+++ src/server/server.js
@@ -95,13 +95,6 @@ app.get('/api/image', async (req, res) => {
 
 
 
-app.use(express.static(path.join(__dirname, '../../../build')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../../build', 'index.html'));
-});
-
-
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
