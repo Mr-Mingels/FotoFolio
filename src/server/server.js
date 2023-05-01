6ee4a125e71eec94ef8b78164fc5082927fc3cb0
Pierre <Pierre.Mingels@protonmail.com>
2023-05-02 00:41:23 +0200

    updated my server side code once again

--- src/server/server.js
+++ src/server/server.js
@@ -95,11 +95,13 @@ app.get('/api/image', async (req, res) => {
 
 
 
-app.use(express.static(path.resolve(__dirname, '../../build')));
+app.use(express.static(path.resolve(__dirname, '..', '..', 'build')));
+
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, '..', '..', 'build', 'index.html'));
 });
+
 
 
 
