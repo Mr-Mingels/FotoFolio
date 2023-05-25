4831db618b76419838b712bfa88fd68b3c25a864
Pierre <Pierre.Mingels@protonmail.com>
2023-05-25 16:30:35 +0200

    succesfully fixed the error and cleaned up my code

--- src/components/Search.js
+++ src/components/Search.js
@@ -87,35 +87,17 @@ const Search = ({ getLikedImages }) => {
                 removeDuplicates(prevImages[2], newImages[2]),
             ];
             });
-            console.log(allImages)
-            console.log(data.hits.length)
-            if (data.hits.length === 0 && allImages[0].length === 0) {
-                console.log('if statement is true')
-            } else {
-                console.log('if statement is false')
-            }
             if (data.hits.length === 0 && allImages[0].length === 0) {
                 setIsLoading(false)
                 setNoResultsFound(true)
               } else {
                 setNoResultsFound(false)
               }
-              console.log(allImages[0].length)
         } catch (error) {
           console.error("Error fetching popular images:", error);
           setIsLoading(false)
         }
       };
-
-      useEffect(() => {
-        if (!allImages[0]) {
-            console.log('images are empty')
-        } else {
-            console.log('images are not empty')
-        }
-        console.log(allImages)
-        console.log(allImages[0].length)
-      },[allImages])
       
       useEffect(() => {
         setAllImages([[], [], []]);
